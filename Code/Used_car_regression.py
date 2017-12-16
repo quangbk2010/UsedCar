0576318f5b9c28aa5a92443f66d8a39a6f2bb3f3
quangbk2010 <n.n.quangbk2010@gmail.com>
2017-12-16 13:43:23 +0900

    400 <= price < 10000

--- Code/Used_car_regression.py
+++ Code/Used_car_regression.py
@@ -157,7 +157,7 @@ class Dataset (Data_preprocessing, DataFrameImputer):
 
             # Remove the data points with price == 0
             #total_dataset = total_dataset[total_dataset["price"] != 0]
-            total_dataset = total_dataset[total_dataset["price"] >= 270]
+            total_dataset = total_dataset[total_dataset["price"] >= 400]
             print ("3.1", total_dataset.shape)
             total_dataset = total_dataset[total_dataset["price"] < 10000]
             #print ("3.2", total_dataset.shape)
