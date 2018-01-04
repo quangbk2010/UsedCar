41fccb942d496e7cb65cfd24c59d1308afa5e6a6
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-01-04 21:17:17 +0900

    Bagging

--- Code/Main.py
+++ Code/Main.py
@@ -19,8 +19,9 @@ if __name__ == '__main__':
     parser.add_argument('--model_set', type=str, default = 'sklearn') # "Sklearn" or "DL"
     parser.add_argument('--dataset_size', type=str, default = 'full') # "full", or "partial", or "small"
     parser.add_argument('--car_ident_flag', type=int, default = 0) # 1-yes, 0-no
-    parser.add_argument('--gb_NN_flag', type=int, default = 0) # 0-no, 1-gb_NN_baseline, 2-gb_NN_car2vect, 3-gb_NN_baseline_tree
+    parser.add_argument('--ensemble_NN_flag', type=int, default = 0) # 0-no, 1-gb_NN_baseline, 2-gb_NN_car2vect, 3-gb_NN_baseline_tree, 4-bagging_NN_baseline
     parser.add_argument('--num_regressor', type=int, default = 0) # the number of regressors used for gradient boosting
+    parser.add_argument('--sample_ratio', type=float, default = 1.0) # the percentage of data sample in the total dataset used in bagging ensemble method
 
 
     ########################
@@ -141,15 +142,19 @@ if __name__ == '__main__':
     elif args.model_set == "DL":
         nn = Tensor_NN (args)
  
-        if args.gb_NN_flag == 1:
+        if args.ensemble_NN_flag == 1:
             nn.gradient_boosting_NN_baseline (train_data=train_data, train_label=train_label, test_data=test_data, test_label=test_label, y_predict_file_name=y_predict_file_name, mean_error_file_name=mean_error_file_name, dataset_size=dataset_size)
 
-        elif args.gb_NN_flag == 2:
+        elif args.ensemble_NN_flag == 2:
             nn.gradient_boosting_NN_car2vect (train_data=train_data, train_label=train_label, test_data=test_data, test_label=test_label, test_car_ident=test_car_ident, d_ident=dataset.d_ident, d_remain=dataset.d_remain, y_predict_file_name=y_predict_file_name, mean_error_file_name=mean_error_file_name, x_ident_file_name=x_ident_file_name, x_embed_file_name=x_embed_file_name, dataset_size=dataset_size)
 
-        elif args.gb_NN_flag == 3:
+        elif args.ensemble_NN_flag == 3:
             num_trees = 2
             nn.gradient_boosting_NN_baseline_Tree (train_data=train_data, train_label=train_label, test_data=test_data, test_label=test_label, y_predict_file_name=y_predict_file_name, mean_error_file_name=mean_error_file_name, dataset_size=dataset_size)
+
+        elif args.ensemble_NN_flag == 4:
+            nn.bagging_NN_baseline (train_data=train_data, train_label=train_label, test_data=test_data, test_label=test_label, y_predict_file_name=y_predict_file_name, mean_error_file_name=mean_error_file_name, dataset_size=dataset_size, ratio=args.sample_ratio)
+
 
         elif nn.car_ident_flag == 1:
             model_path = nn.model_dir + "/car2vect/" + "[" + dataset_size + "]" + nn.model_name  + "_" + args.label  + "_car2vect_" + str (nn.no_neuron) + "_" + str (nn.no_neuron_embed) + "_" + str (nn.d_embed) + "_" + nn.loss_func
--- Code/run.sh
+++ Code/run.sh
@@ -6,6 +6,23 @@
 #python Neural_network_tensorflow.py --no_neuron 10000 --no_hidden_layer 1 --epoch 20 --scale_label 1
 #python Neural_network_tensorflow.py --no_neuron 1000 --no_neuron_embed 1000 --epoch 50 --scale_label 1
 
-#python Main.py --model_set DL --dataset_size full --gb_NN_flag 1 --loss_func mse --car_ident_flag 0 --no_neuron 1000 --no_hidden_layer 1 --epoch 30 --num_regressor 3
-#python Main.py --model_set DL --dataset_size full --gb_NN_flag 2 --loss_func mse --car_ident_flag 1 --no_neuron 1000  --no_neuron_embed 6000 --epoch 30 --num_regressor 3
-python Main.py --model_set DL --dataset_size full --gb_NN_flag 3 --loss_func mse --car_ident_flag 0 --no_neuron 1000  --no_hidden_layer 2 --epoch 30 --num_regressor 3
+#python Main.py --model_set DL --dataset_size full --ensemble_NN_flag 1 --loss_func mse --car_ident_flag 0 --no_neuron 1000 --no_hidden_layer 1 --epoch 30 --num_regressor 3
+#python Main.py --model_set DL --dataset_size full --ensemble_NN_flag 2 --loss_func mse --car_ident_flag 1 --no_neuron 1000  --no_neuron_embed 6000 --epoch 30 --num_regressor 3
+#python Main.py --model_set DL --dataset_size full --ensemble_NN_flag 3 --loss_func mse --car_ident_flag 0 --no_neuron 1000  --no_hidden_layer 2 --epoch 30 --num_regressor 3
+python Main.py --model_set DL --dataset_size full --ensemble_NN_flag 4 --car_ident_flag 0 --no_neuron 1000  --no_hidden_layer 2 --epoch 30 --num_regressor 3 --sample_ratio 0.5
+
+#banana2
+cd ~/UsedCar/Code/Dataframe/
+scp \[full\]total_dataframe_Initial.h5 quang@banana1.kaist.ac.kr:~/UsedCar/Code/Dataframe
+
+cd ~/UsedCar/checkpoint/gb_NN/baseline/regressor1
+scp checkpoint full_usedCar_price_baseline_2000_2_rel_err_9.* quang@banana4.kaist.ac.kr:~/UsedCar/checkpoint/gb_NN/baseline/regressor1
+
+#client host
+cd ~/UsedCar/checkpoint
+mkdir -p gb_NN/baseline
+cd gb_NN/baseline
+mkdir regressor1 regressor2 regressor3 regressor4 regressor5 regressor6 regressor7 regressor8 regressor9 regressor10 
+
+
+
