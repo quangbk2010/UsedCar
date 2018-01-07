9a960010e02f6ca91154d41531833c3b89456cdc
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-01-07 15:54:36 +0900

    GB acr2vect

--- Code/Models.py
+++ Code/Models.py
@@ -679,7 +679,7 @@ class Tensor_NN (Dataset, Sklearn_model):
 
                 # Save predicted label and determine the best epoch
                 if loss_func == "rel_err":
-                    threshold_err = 10 #8.5
+                    threshold_err = 9 #8.5
                     epoch_test_err_val = epoch_test_relative_err_val
                 elif loss_func == "mae":
                     threshold_err = 150
