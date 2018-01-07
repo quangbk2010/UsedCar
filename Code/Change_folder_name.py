de6e60c20483d5aba6f6b190cb19b64543063802
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-01-07 22:49:47 +0900

    Bagging

--- Code/Change_folder_name.py
+++ Code/Change_folder_name.py
@@ -5,7 +5,7 @@ parser = argparse.ArgumentParser()
 
 parser.add_argument('--model', type=str, default = 'gb_NN') #"bagging_NN", "gb_NN"
 parser.add_argument('--nn_type', type=str, default = 'car2vect') # "baseline", "car2vect"
-parser.add_argument('--new_folder', type=str, default = '"saved_model_remove_price_smaller_200_bigger_9000_sortby_adv_date__car2vect_6000_1000_sampleRatio_50"') # name of new folder
+parser.add_argument('--new_folder', type=str, default = 'saved_model_remove_price_smaller_200_bigger_9000_sortby_adv_date__car2vect_6000_1000')#_sampleRatio_50') # name of new folder
 
 args = parser.parse_args()
 
@@ -23,5 +23,5 @@ else:
 for i in range (num_regressors):
     bash_cmd = "cd " + path + str (i+1) + ";"
     bash_cmd += "rm full_*;"
-    bash_cmd += "cp -r temp_save " + new_folder
+    bash_cmd += "mv temp_save " + new_folder
     os.system (bash_cmd)
