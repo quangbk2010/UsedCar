940d6a401ab5ff911814ce32c81af9913b2d4dee
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-01-07 16:20:34 +0900

    GB acr2vect

--- Code/Models.py
+++ Code/Models.py
@@ -685,7 +685,7 @@ class Tensor_NN (Dataset, Sklearn_model):
                     threshold_err = 150
                     epoch_test_err_val = epoch_test_mae_val
                 elif loss_func == "mse":
-                    threshold_err = 140
+                    threshold_err = 200 #140
                     epoch_test_err_val = epoch_test_rmse_val
 
                 #if (epoch + 1) % 1 == 0:
