7627b9a5140994338fe1c59f3a8587a3fd1e608d
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-02-27 20:45:47 +0900

    t

--- Code/Dataset.py
+++ Code/Dataset.py
@@ -162,7 +162,8 @@ class Dataset ():
 
         if os.path.isfile (dataframe_file) == False:
             print ("Load dataset from excel file")
-            total_dataset = pd.read_excel (dataset_excel_file, names = self.headers)#, converters = dtype_dict, header = 0, encoding='utf-8')
+            total_dataset = pd.read_excel (dataset_excel_file)#, names = self.headers)#, converters = dtype_dict, header = 0, encoding='utf-8')
+            sys.exit (-1)
             # Shuffle dataset (dataframe)
             #total_dataset = total_dataset.reindex(np.random.permutation(total_dataset.index))
             
