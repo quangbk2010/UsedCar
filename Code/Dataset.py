fb5056b6fbb5ac1a86b3eb318834dbe1c24ba279
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-03-23 10:12:37 +0900

    rm outliers

--- Code/Dataset.py
+++ Code/Dataset.py
@@ -222,8 +222,8 @@ class Dataset ():
                 sales_duration = self.get_sale_duration_array (total_dataset)
                 total_dataset ["sale_duration"] = sales_duration
 
-                #total_dataset = total_dataset[np.abs(total_dataset["sale_duration"] - total_dataset["sale_duration"].mean()) / total_dataset["sale_duration"].std() < 3]
-                #print ("4.4", total_dataset.shape)
+                total_dataset = total_dataset[np.abs(total_dataset["sale_duration"] - total_dataset["sale_duration"].mean()) / total_dataset["sale_duration"].std() < 1]
+                print ("4.4", total_dataset.shape)
 
             # Just keep hyundai and kia
             #total_dataset = total_dataset[(total_dataset["maker_code"] == 101) | (total_dataset["maker_code"] == 102)]
