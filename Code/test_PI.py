716456939a66112745b2615a3e150248e55cc28d
quangbk2010 <n.n.quangbk2010@gmail.com>
2018-04-25 11:32:34 +0900

    test

--- Code/test_PI.py
+++ Code/test_PI.py
@@ -1,3 +1,4 @@
+from __future__ import division
 import numpy as np
 import pandas as pd
 
@@ -7,6 +8,7 @@ np.set_printoptions (threshold = np.nan)
 pd.set_option('display.max_columns', None)
 import math
 import scipy.stats as stats
+from tqdm import tqdm
 
 def compute_cwc (data, gama1, gama2, eta=10, muy=0.95, alpha1=0, alpha2=0):
     len_data = len (data)
@@ -24,7 +26,7 @@ def compute_cwc (data, gama1, gama2, eta=10, muy=0.95, alpha1=0, alpha2=0):
     
     return (gama1, gama2, eta, alpha1, alpha2, nmpiw, picp, muy, cwc)
 
-def determine_pred_interval (eta=10, percent_threshold=0.95):
+def read_data ():
     data = pd.read_csv ("../Results/price.txt", sep="\t")
     len_data = len (data)
     len1 = int (len_data / 2)
@@ -33,14 +35,25 @@ def determine_pred_interval (eta=10, percent_threshold=0.95):
     # Divide it into 2 part: validation set and test set
     data1 = data [:len1]
     data2 = data [len1:]
+    return data1, data2
+
+def determine_pred_interval (data1, eta=10, percent_threshold=0.95):
     muy = percent_threshold # eta: 10, 100, 300
     res = np.empty ((0, 9))
     # Determine the range that ground truth included in the range from the validation set   
-    for gama1 in np.arange (0.05, 0.25, 0.01):#np.arange (0.05, 0.3, 0.05):
-        for gama2 in np.arange (0.05, 0.25, 0.01):#np.arange (0.05, 0.3, 0.05):
-            for alpha1 in np.arange (0, 5.1, 0.5):
-                for alpha2 in np.arange (0, 5.1, 0.5):
+    """for gama1 in np.arange (0.05, 0.26, 0.01):#np.arange (0.05, 0.3, 0.05):
+        for gama2 in np.arange (0.05, 0.07, 0.01):#np.arange (0.05, 0.3, 0.05):
+            for alpha1 in np.arange (0, 1.1, 0.5):
+                for alpha2 in np.arange (0, 1.1, 0.5):"""
+    a = np.arange (0.01, 0.26, 0.01)
+    b = np.arange (0, 5.1, 0.5)
+
+    for gama1 in  tqdm (a):# ([0.05, 0.07, 0.01]):#np.arange (0.05, 0.3, 0.05):
+        for gama2 in tqdm (a):# ([0.05, 0.07, 0.01]):#np.arange (0.05, 0.3, 0.05):
+            for alpha1 in tqdm (b):# ([0, 1.1, 0.5]):
+                for alpha2 in tqdm (b):# ([0, 1.1, 0.5]):
                     res = np.vstack ([res, compute_cwc (data1, gama1, gama2, eta, muy, alpha1, alpha2)])
+
     res = res[res[:, -3] > muy]
     idx = np.argmin (res[:, -1])
     return res[idx].reshape (-1, 9)
@@ -48,9 +61,10 @@ def determine_pred_interval (eta=10, percent_threshold=0.95):
 res_arr = np.empty ((0, 9))
 
 # Valdation step 
-for eta in [1, 5, 10]:
-    for muy in [0.95]:#0.8, 0.85, 0.9, 0.95, 0.99]:
-        res = determine_pred_interval (eta, muy)
+data1, data2 = read_data ()
+for eta in tqdm ([1, 5, 10]):
+    for muy in [0.85]:#, 0.85, 0.9, 0.95, 0.99]:
+        res = determine_pred_interval (data1, eta, muy)
         #print (res)
         res_arr = np.concatenate ((res_arr, res), axis=0)
 np.savetxt ("./baseline_PIs.txt", res_arr, fmt="%.2f\t%.2f\t%.2f\t%.2f\t%.2f\t%.4f\t%.4f\t%.2f\t%.7f")
